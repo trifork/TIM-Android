e5a7bf686315f26c1120264d12372292c42e4450
Marc le Fevre Johansen <mfj@trifork.com>
2021-08-23 15:42:40 +0200

    Initial android lib commit

---
+++ TIM-Android/src/main/java/com/trifork/tim/models/TIMConfiguration.kt
@@ -0,0 +1,18 @@
+package com.trifork.tim.models
+
+import android.net.Uri
+
+
+
+class TIMConfiguration(
+    val timBaseUrl: String,
+    val realm: String,
+    val clientId: String,
+    val redirectUri: Uri,
+    val scopes: List<String>,
+    // TODO: Add encryption method as constructor param - MFJ
+    // TODO: Add key service version param - MFJ
+) {
+
+
+}
